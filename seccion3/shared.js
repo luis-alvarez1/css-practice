cd0db314705a73037c0733cb204beaf5b8877421
Luis Alvarez <lealvarezp@unac.edu.co>
2023-01-04 19:03:39 -0500

    animations

--- seccion3/shared.js
+++ seccion3/shared.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const cancelButton = document.querySelector('.modal__action--negative');
 const mobileNav = document.querySelector('.mobile-nav');
 const toggleButton = document.querySelector('.toggle-button');
+const ctaButton = document.querySelector('.main-nav__item--cta');
 
 if (selectPlanButtonList) {
   for (let i = 0; i < selectPlanButtonList.length; i++) {
@@ -40,4 +41,14 @@ toggleButton?.addEventListener('click', () => {
   setTimeout(() => {
     backdrop?.classList.add('visible');
   }, 10);
+});
+
+ctaButton.addEventListener('animationstart', (event) => {
+  console.log('animation started', event);
+});
+ctaButton.addEventListener('animationend', (event) => {
+  console.log('animation ended', event);
+});
+ctaButton.addEventListener('animationiteration', (event) => {
+  console.log('animation iterated', event);
 });
